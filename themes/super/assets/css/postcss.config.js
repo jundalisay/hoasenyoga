14f407bba43b4f02f754c64bc3cbe651e143dd31
“jundalisay” <jundalisay@yahoo.com>
2025-07-26 08:14:48 +0800

    update

--- themes/super/assets/css/postcss.config.js
+++ themes/super/assets/css/postcss.config.js
@@ -12,6 +12,20 @@
 //   ],
 // };
 
-export default {
-  plugins: ["@tailwindcss/postcss"],
-}
+
+// postcss.config.js (v4)
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    // Autoprefixer is now often built-in to Tailwind CSS v4, 
+    // so you might not need to explicitly include it anymore.
+    // However, if you encounter issues, you can add it back:
+    // autoprefixer: {}, 
+  },
+};
+
+// export default {
+//   plugins: ["@tailwindcss/postcss"]
+// }
+
+
